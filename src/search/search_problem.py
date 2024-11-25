f0a6d3b8412300138ac8f91f7c4166423bc27fa0
Pedro Pinto <pmap@ua.pt>
2024-11-25 13:28:04 +0000

    change how to check if a goal is reached

--- src/search/search_problem.py
+++ src/search/search_problem.py
@@ -13,7 +13,11 @@ class SearchProblem:
     def __init__(self, domain: SearchDomain, initial, goal):
         self.domain = domain
         self.initial = initial
-        self.goal = goal
+        self.goals = goals
+        self.ongoing_goals = goals.copy()
+    
+    def final_goal_test(self, state):
+        return all(self.domain.satisfies(state, goal) for goal in self.goals)
     
     def goal_test(self, state):
-        return self.domain.satisfies(state,self.goal)
+        return self.domain.satisfies(state, self.ongoing_goals[0])
